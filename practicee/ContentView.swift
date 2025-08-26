8e576b831a9df5394bc08291ef58e314e6117bbd
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-08-26 13:35:50 -0600

    Initial Commit

---
+++ practicee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  practicee
+//
+//  Created by Benjamin Rojo on 26/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ practicee/practiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceeApp.swift
+//  practicee
+//
+//  Created by Benjamin Rojo on 26/08/25.
+//
+
+import SwiftUI
+
+@main
+struct practiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
